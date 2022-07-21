dd1734a6d9a2b14c0de9cb911ec6914f6e3d4a70
dominicstuder <dominicstuder@users.noreply.github.com>
2022-07-21 14:41:51 +1000

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `By and Large Studio`,
+    description: `A strategy, design and technology studio, specialising in transforming brands.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    image: `src/images/share-image.png`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-react-router-scroll`,
+    `react-pose`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,6 +17,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -23,16 +26,50 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#ffca41`,
+        theme_color: `#ffca41`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        custom:{
+          families: ["UntitledSans-Regular", "UntitledSans-Light"],
+          urls: ["/fonts/fonts.css"],
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-scroll-reveal`,
+      options: {
+          threshold: 0.1, // Percentage of an element's area that needs to be visible to launch animation
+          once: true, // Defines if animation needs to be launched once
+          disable: false, // Flag for disabling animations
+          
+          // Advanced Options
+          selector: '[data-sal]', // Selector of the elements to be animated
+          animateClassName: 'sal-animate', // Class name which triggers animation
+          disabledClassName: 'sal-disabled', // Class name which defines the disabled state
+          rootMargin: '0% 50%', // Corresponds to root's bounding box margin
+          enterEventName: 'sal:in', // Enter event name
+          exitEventName: 'sal:out', // Exit event name
+      },
+    },
+    {
+      resolve: "gatsby-plugin-transition-link",
+      options: {
+        // layout: require.resolve(`./src/components/header.js`)
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/ // See below to configure properly
+        }
+      }
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,122 +1,379 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React, { useState, useEffect} from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
-import * as styles from "../components/index.module.css"
+import SEO from "../components/seo"
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-  },
-  {
-    text: "Examples",
-    url: "https://github.com/gatsbyjs/gatsby/tree/master/examples",
-    description:
-      "A collection of websites ranging from very basic to complex/complete that illustrate how to accomplish specific tasks within your Gatsby sites.",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Learn how to add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
+import { Container, Row, Col } from 'react-grid-system';
+import Arrow from "../assets/arrow.svg";
+import ArrowWhite from "../assets/arrow-small-white.svg";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+// import clip from '../assets/feature_7.mp4'; 
+// import astridthumb from '../assets/astrid_thumb.mp4'; 
+// import dcthumb from '../assets/dc_thumb2.mp4'; 
+import { setConfiguration } from 'react-grid-system';
+setConfiguration({ gutterWidth:'60'});
 
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
 
-const moreLinks = [
-  { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
-  {
-    text: "Documentation",
-    url: "https://gatsbyjs.com/docs/",
-  },
-  {
-    text: "Starters",
-    url: "https://gatsbyjs.com/starters/",
-  },
-  {
-    text: "Showcase",
-    url: "https://gatsbyjs.com/showcase/",
-  },
-  {
-    text: "Contributing",
-    url: "https://www.gatsbyjs.com/contributing/",
-  },
-  { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
+const IndexPage = () => {
+  return (
+    <Layout style={{
+      backgroundColor: '#F2E4CC',
+    }}>
+      <SEO title="Home" />
 
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+      <Container fluid className="video-container">
+      <Row justify="start" style={{
+                 marginTop: '4em',
+                }} className="video-section">
+                  <Col md={12} lg={8} xl={8} xxl={6}  style={{
+                    position: 'relative',
+                    marginTop: '5em',
+                  }}
+                  className="video-container">
+          
+        
+                  <div className="video-wrapper delay-2" style={{
+                        
+                        }}>
+                           <AniLink
+                    to="/work"
+                    exit={{ length: 0.5 }}
+                    entry={{ delay: 0.5 }}
+                  >     
+                    {/* <video autoPlay loop muted className="background-video">
+                            <source src={clip} type='video/mp4' />
+                          
+                    </video> */}
+                    </AniLink>
+                  </div>
+      
+                 </Col>
+                </Row>
+                 
+             <Row justify="start" style={{
+                 marginTop: '10em',
+                }} className="home-page-intro">
+                  <Col sm={12} lg={4} xl={4} xxl={4} className="body">About us</Col>
+                   <Col sm={12} md={6} lg={8} xl={8} xxl={6} style={{
+                      position: 'relative',
+              
+                    }}> <h1 
+                    className="body delay-1"><div       
+                    data-sal="slide-up"
+                    data-sal-delay="100"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">By and Large is an independent,
+                   multidisciplinary</div> 
+                   <div   data-sal="slide-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">design studio. We create positive impact</div> 
+                     <div   data-sal="slide-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">through creative and considered strategy,</div>
+                     <div   data-sal="slide-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">
+                     design, content creation and use
+                    </div>
+                    <div   data-sal="slide-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">
+                    of technology.
+                    </div>
+                    </h1>
+                    <h1 
+                    className="body delay-1"><div       
+                    data-sal="slide-up"
+                    data-sal-delay="100"
+                    data-sal-easing="ease"
+                    data-sal-duration="500"
+                    className="mt1 text-indent"
+                    >    Currently based in Newcastle, NSW. We work </div>
+                   <div   data-sal="slide-up"
+                    data-sal-delay="300"
+                    data-sal-easing="ease"
+                    data-sal-duration="500">with clients around Australia and beyond.</div> 
+                  
+                    </h1>
+                      </Col>
+                  </Row>
+                
+          </Container>
+          <Container fluid className="section-3">
+              <Row style={{paddingTop: '1em',
+                  position: 'relative'
+                }}>
+                {/* <Col  md={12} lg={12} xl={12} xxl={12}  className="delay-4">
+                  <h3 className="small-body-copy">Capabilities</h3>
+                </Col> */}
+                <Col  md={12} lg={12} xl={12} xxl={12}  className="delay-5 copy-block pb3"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
-)
+              >
+                <div 
+                  data-sal="slide-up"
+                  data-sal-delay="100"
+                  data-sal-easing="ease"
+                  data-sal-duration="500"
+                >
+                  <h3 className="body">What we do</h3>
+                  </div>
+                </Col>
+                <Col md={12} lg={4} xl={4} xxl={4}
+               
+                >
+                      <div                   
+                      data-sal="slide-up"
+                        data-sal-delay="100"
+                        data-sal-easing="ease"
+                        data-sal-duration="500"
+                        >
+                  <ul className="services-list delay-5">
+                      <li>Strategy</li>
+                      <li>Brand Strategy</li>
+                      <li>UX Strategy</li>
+                      <li>Information Architecture</li>
+                      <li>Content Strategy</li>
+                      <li>Business Model Design</li>
+                      </ul>
+                      </div>
+                </Col>
+                <Col md={12} lg={4} xl={4} xxl={4} >
+                  <div                   
+                      data-sal="slide-up"
+                        data-sal-delay="200"
+                        data-sal-easing="ease"
+                        data-sal-duration="500"
+                        >
+                <ul className="services-list delay-5"
+      
+                >
+                      <li>Design</li>
+                      <li>Creative Direction</li>
+                      <li>UX/UI Design</li>
+                      <li>Prototyping</li>
+                      <li>Graphic Design</li>
+                      <li>Content Design</li>
+                      </ul>
+                      </div>
+                </Col>
+                <Col md={12} lg={4} xl={4} xxl={4} >
+                <div                   
+                      data-sal="slide-up"
+                        data-sal-delay="300"
+                        data-sal-easing="ease"
+                        data-sal-duration="500"
+                        >
+                <ul className="services-list delay-5">
+                      <li>Technology</li>
+                      <li>Front-end Development</li>
+                      <li>Ecommerce</li>
+                      <li>Content management systems</li>
+
+                      </ul>
+                      </div>
+                </Col>
+                <Col md={12} lg={12} xl={12} xxl={12} >
+                <div className="primary-button delay-1 mt5"><AniLink
+                          to="/contact"
+                          exit={{ length: 0.5 }}
+                          entry={{ delay: 0.5 }}
+                        ><ArrowWhite className="small-arrow"></ArrowWhite><span className="link-text">Contact us</span></AniLink></div>
+                  </Col>
+            </Row>
+      </Container>
+      {/* <Container fluid>
+        <Row justify="between" style={{
+            position: 'relative',
+            paddingTop: '15em',
+          }}>
+            <Col sm={12} md={12} lg={10} xl={10} xxl={12} style={{
+            position: 'relative',
+    
+          }}> <h1 
+          className="intro-copy delay-1">A strategy, design and technology studio, specialising in transforming brands.</h1>
+            </Col>
+          
+          </Row>
+      </Container>
+      <Container fluid className="video-container">
+            <Row justify="between" style={{
+                 
+                }} className="video-section">
+                  <Col md={12} lg={10} xl={10} xxl={10}  style={{
+                    position: 'relative'
+                  }}
+                  className="video-container">
+          
+        
+                  <div className="video-wrapper delay-2" style={{
+                        
+                        }}>
+                    <video autoPlay loop muted className="background-video">
+                            <source src={clip} type='video/mp4' />
+                    </video>
+                  </div>
+                </Col>
+              </Row>
+            </Container> */}
+       
+      <Container fluid className="section-4">
+            <Row justify="between" style={{
+              paddingTop: '1em',
+              position: 'relative',
+            }}>
+                {/* <Col  md={12} lg={12} xl={12} xxl={12}  className="delay-3">
+                  <h3 className="small-body-copy">Recent case studies</h3>
+                </Col> */}
+                <Col sm={12} md={12} lg={10} xl={10} xxl={12} style={{
+            position: 'relative',
+    
+          }}> 
+          <div 
+            data-sal="slide-up"
+            data-sal-delay="200"
+            data-sal-easing="ease"
+            data-sal-duration="500"
+            >
+          <h1 
+          className="intro-copy delay-1">Some of our recent work</h1>
+          </div>
+            </Col>
+              {/* <Col  md={12} lg={12} xl={12} xxl={12}  className="delay-3 copy-block">
+                  <h3 className="header-3">Recent work</h3>
+                </Col> */}
+              </Row>
+              <div className="work-list">
+              <Row style={{
+                position: 'relative',
+              }}>
+                        {/* <AniLink
+                          cover  to="/work" bg="#1E1E1E"
+                          direction="left"
+                          duration={1.5}
+                        ><div className="item">
+                               
+                             <div className="video-wrapper-small" style={{
+                                
+                              }}>
+                            <video autoPlay loop muted className="background-video">
+                                    <source src={clip} type='video/mp4' />
+                            </video>
+                          </div>
+                            <Col sm={12} md={12} lg={12} xl={12} xxl={12} style={{
+                              position: 'relative'
+                              }}>
+                          <div className="inner">Master Me<Arrow className="arrow"></Arrow></div>
+
+                          </Col>
+                          
+                          </div></AniLink>
+               
+             */}
+              <AniLink
+                    to="/astrid"
+                    exit={{ length: 0.5 }}
+                    entry={{ delay: 0.5 }}
+                  >
+                                    <div 
+                    data-sal="slide-up"
+                    data-sal-delay="200"
+                    data-sal-easing="ease"
+                    data-sal-duration="500"
+                    >   
+                          <div className="item delay-2">
+                          <div className="video-wrapper-small" style={{
+                                
+                              }}>
+                            {/* <video autoPlay loop muted className="background-video">
+                                    <source src={astridthumb} type='video/mp4' />
+                            </video> */}
+                          </div>
+                            <Col sm={12} md={12} lg={12} xl={12} xxl={12} style={{
+                              position: 'relative'
+                              }}>
+                          <div className="inner">Astrid<Arrow className="arrow"></Arrow></div>
+                          </Col>
+                          </div>
+                          </div>
+                  </AniLink>
+                   <AniLink
+                    to="/drive-change"
+                    exit={{ length: 0.5 }}
+                    entry={{ delay: 0.5 }}
+                  >     
+                    <div 
+                    data-sal="slide-up"
+                    data-sal-delay="200"
+                    data-sal-easing="ease"
+                    data-sal-duration="500"
+                    >   
+                          <div className="item delay-2">
+                          <div className="video-wrapper-small" style={{
+                                
+                              }}>
+                            {/* <video autoPlay loop muted className="background-video">
+                                    <source src={dcthumb} type='video/mp4' />
+                            </video> */}
+                          </div>
+                            <Col sm={12} md={12} lg={12} xl={12} xxl={12} style={{
+                              position: 'relative'
+                              }}>
+                          <div className="inner">Drive change<Arrow className="arrow"></Arrow></div>
+                          </Col>
+                          </div>
+                     </div>
+                  </AniLink>
+                  <AniLink
+                    to="/drive-change"
+                    exit={{ length: 0.5 }}
+                    entry={{ delay: 0.5 }}
+                  >     
+                    <div 
+                    data-sal="slide-up"
+                    data-sal-delay="200"
+                    data-sal-easing="ease"
+                    data-sal-duration="500"
+                    >   
+                          <div className="item delay-2">
+                          <div className="video-wrapper-small" style={{
+                                
+                              }}>
+                            {/* <video autoPlay loop muted className="background-video">
+                                    <source src={dcthumb} type='video/mp4' />
+                            </video> */}
+                          </div>
+                            <Col sm={12} md={12} lg={12} xl={12} xxl={12} style={{
+                              position: 'relative'
+                              }}>
+                          <div className="inner">Great Forest Trail Marathon<Arrow className="arrow"></Arrow></div>
+                          </Col>
+                          </div>
+                     </div>
+                  </AniLink>
+                  </Row>         
+                  </div>
+                  <div 
+                    data-sal="slide-up"
+                    data-sal-delay="200"
+                    data-sal-easing="ease"
+                    data-sal-duration="500"
+                    >   
+                  <Col md={12} lg={12} xl={12} xxl={12} >
+                <div className="primary-button delay-1 mt5"><AniLink
+                          to="/work"
+                          exit={{ length: 0.5 }}
+                          entry={{ delay: 0.5 }}
+                        ><ArrowWhite className="small-arrow"></ArrowWhite><span className="link-text">View all our work</span></AniLink></div>
+                  </Col>
+                  </div>
+        </Container>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
