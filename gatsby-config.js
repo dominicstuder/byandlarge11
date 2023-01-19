bcfb8e441a687fbc65cc71c4b3a40df3fb39360a
dominicstuder <dominicstuder@users.noreply.github.com>
2023-01-19 19:20:05 +1100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-transition-link",
           options: {
-              layout: require.resolve(`./src/components/Layout.js`)
+              layout: require.resolve(`./src/components/layout.js`)
               
             }
     },
